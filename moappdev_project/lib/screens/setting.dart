1e856d3c7990548cd57b5f125c2d080226843e32
SuminJN <wjstnals1211@handong.ac.kr>
2022-05-27 00:47:11 +0900

    Revert "No conflict"
    This reverts commit ac1ad8ad1b19d2162d30be469e538466cc67cede.

--- moappdev_project/lib/screens/setting.dart
+++ moappdev_project/lib/screens/setting.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:user_profile_avatar/user_profile_avatar.dart';
 
 class SettingPage extends StatefulWidget {
   const SettingPage({Key? key}) : super(key: key);
@@ -11,15 +10,9 @@ class SettingPage extends StatefulWidget {
 class _SettingPageState extends State<SettingPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: ListView(
-        padding: const EdgeInsets.all(8),
-        children: <Widget>[
-          Container(
-            height: 50,
-            child: ,
-          ),
-        ],
+    return const Scaffold(
+      body: Center(
+        child: Text('This is Setting Page'),
       ),
     );
   }

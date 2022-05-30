0a5030a6919b95c8a765a0f2464cb297fbf18bf8
SuminJN <wjstnals1211@handong.ac.kr>
2022-05-30 16:00:24 +0900

    Revert "firebase update"
    This reverts commit 5e84a0ee93e541202a221bce76ba00897beae176.

--- moappdev_project/lib/login.dart
+++ moappdev_project/lib/login.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:moappdve_project/signup.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:moappdve_project/utils.dart';
+import 'authentication.dart';
 
 import 'home.dart';
 
--- moappdev_project/lib/screens/calendar/calendar.dart
+++ moappdev_project/lib/screens/calendar/calendar.dart
@@ -11,10 +11,14 @@ class TableEventsExample extends StatefulWidget {
 }
 
 class _TableEventsExampleState extends State<TableEventsExample> {
-  late ValueNotifier<List<Event>> _selectedEvents;
+  late final ValueNotifier<List<Event>> _selectedEvents;
   CalendarFormat _calendarFormat = CalendarFormat.month;
+  RangeSelectionMode _rangeSelectionMode = RangeSelectionMode
+      .toggledOff;
   DateTime _focusedDay = DateTime.now();
   DateTime? _selectedDay;
+  DateTime? _rangeStart;
+  DateTime? _rangeEnd;
 
   @override
   void initState() {
@@ -24,6 +28,7 @@ class _TableEventsExampleState extends State<TableEventsExample> {
     _selectedDay = _focusedDay;
     _selectedEvents = ValueNotifier(_getEventsForDay(_selectedDay!));
   }
+
 
   @override
   void dispose() {
@@ -35,18 +40,50 @@ class _TableEventsExampleState extends State<TableEventsExample> {
     return events[day] ?? [];
   }
 
+  List<Event> _getEventsForRange(DateTime start, DateTime end) {
+    // Implementation example
+    final days = daysInRange(start, end);
+
+    return [
+      for (final d in days) ..._getEventsForDay(d),
+    ];
+  }
+
   void _onDaySelected(DateTime selectedDay, DateTime focusedDay) {
     if (!isSameDay(_selectedDay, selectedDay)) {
       setState(() {
         _selectedDay = selectedDay;
         _focusedDay = focusedDay;
+        _rangeStart = null; // Important to clean those
+        _rangeEnd = null;
+        _rangeSelectionMode = RangeSelectionMode.toggledOff;
       });
 
       _selectedEvents.value = _getEventsForDay(selectedDay);
     }
   }
 
-  Stream planStream = FirebaseFirestore.instance.collection('plan').snapshots();
+  void _onRangeSelected(DateTime? start, DateTime? end, DateTime focusedDay) {
+    setState(() {
+      _selectedDay = null;
+      _focusedDay = focusedDay;
+      _rangeStart = start;
+      _rangeEnd = end;
+      _rangeSelectionMode = RangeSelectionMode.toggledOn;
+    });
+
+    // `start` or `end` could be null
+    if (start != null && end != null) {
+      _selectedEvents.value = _getEventsForRange(start, end);
+    } else if (start != null) {
+      _selectedEvents.value = _getEventsForDay(start);
+    } else if (end != null) {
+      _selectedEvents.value = _getEventsForDay(end);
+    }
+  }
+
+  Stream planStream =
+  FirebaseFirestore.instance.collection('plan').snapshots();
 
   @override
   Widget build(BuildContext context) {
@@ -58,13 +95,18 @@ class _TableEventsExampleState extends State<TableEventsExample> {
             lastDay: kLastDay,
             focusedDay: _focusedDay,
             selectedDayPredicate: (day) => isSameDay(_selectedDay, day),
+            rangeStartDay: _rangeStart,
+            rangeEndDay: _rangeEnd,
             calendarFormat: _calendarFormat,
+            rangeSelectionMode: _rangeSelectionMode,
             eventLoader: _getEventsForDay,
+            // eventLoader: (day) => _getEventsForDay(day),
             startingDayOfWeek: StartingDayOfWeek.sunday,
             calendarStyle: const CalendarStyle(
               outsideDaysVisible: false,
             ),
             onDaySelected: _onDaySelected,
+            onRangeSelected: _onRangeSelected,
             onFormatChanged: (format) {
               if (_calendarFormat != format) {
                 setState(() {
@@ -107,4 +149,5 @@ class _TableEventsExampleState extends State<TableEventsExample> {
       ),
     );
   }
+
 }
